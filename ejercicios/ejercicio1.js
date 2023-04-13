e3b63c4fe20d912de490cde09f632797966b9f27
andrea.pennisi <andrea.pennisi@connect.auto>
2023-04-13 15:58:00 +0100

    ejercicio de clase1

---
+++ ejercicios/ejercicio1.js
@@ -0,0 +1,15 @@
+const data = {
+  nombre: "andrea",
+  edad: 41,
+  precio: 200,
+  nombreDeSeries: ["Mandalorian", "Star wars", "Sailor Moon"],
+  nombeDePeliculas: ["Gladietor", "The Grates Showman", "Encanto"],
+};
+
+console.log(data);
+
+edad++;
+nombreDeSeries.push("Friends");
+
+console.log(data.edad);
+console.log(data.nombreDeSeries);
